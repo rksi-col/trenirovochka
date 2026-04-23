d6a29b4b9f6b34cab7e14e040b3761e709b65459
froqb <puten33hokage@gmail.com>
2026-04-24 01:50:19 +0300

    adding exercisesList

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { getAvailableExercises, createTraining } from './api/trainings';
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, Link, useNavigate, useParams } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
